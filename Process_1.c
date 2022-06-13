915c0b3c6427755b3f43d8838b63450e9535edf3
Jiwook Kim <60651346+jiwook021@users.noreply.github.com>
2022-06-12 23:40:52 -0700

    Update Process_1.c

--- Process_1.c
+++ Process_1.c
@@ -8,58 +8,72 @@
 
 #define MAX 100
 
-
 void  INThandler(int);
 
-  
-// structure for message queue
-struct mesg_buffer {
+
+struct mesg_buffer1 {
     long mesg_type;
     char mesg_text[100];
     int number;
-} message;
+} message1;
+  
+  
+  struct mesg_buffer2 {
+    long mesg_type;
+    char mesg_text[100];
+    int number;
+} message2;
   
 
- int msgid;
+int msgid1,msgid2;
   
 int main()
 {
-    key_t key;
+    key_t key1,key2;
    
-    msgctl(msgid, IPC_RMID, NULL);
-  
+    msgctl(msgid1, IPC_RMID, NULL);
     // ftok to generate unique key
-    key = ftok("progfile", 65);
-  
+    key1 = ftok("progfile1", 65);
     // msgget creates a message queue
-    // and returns identifier
-    msgid = msgget(key, 0666 | IPC_CREAT);
-    message.mesg_type = 1;
-  
+    msgid1 = msgget(key1, 0666 | IPC_CREAT);
 
-  message.number = 0;
+
+
+
+    msgctl(msgid2, IPC_RMID, NULL);
+    // ftok to generate unique key
+    key2 = ftok("progfile2", 66);
+    // msgget creates a message queue
+    msgid2 = msgget(key2, 0666 | IPC_CREAT);
+
+
+
+
+    message1.mesg_type = 1;
+    message1.number = 1;
+  
   while(1)
   {
-    message.number++;
-    msgsnd(msgid, &message, sizeof(message), 0); // msgsnd to send message
-    sleep(2);
+    sleep(1);
     
-    msgrcv(msgid, &message, sizeof(message), 1, 0);  // msgrcv to receive message
-    printf("Data Received is : %d \n", message.number); // display the message
+    message1.number++;
+    msgsnd(msgid1, &message1, sizeof(message1), 0); // msgsnd to send message
+   
     
-
+    msgrcv(msgid2, &message2, sizeof(message2), 1, 0);  // msgrcv to receive message
+    printf("Data Received is : %d \n", message2.number); // display the message
     signal(SIGINT, INThandler);
-    
   }
-  msgctl(msgid, IPC_RMID, NULL);
-  
+ 
+  msgctl(msgid1, IPC_RMID, NULL);
+  msgctl(msgid2, IPC_RMID, NULL);
     return 0;
 }
 
 void INThandler(int sig)
 {
-
   signal(sig, SIG_IGN);
-  msgctl(msgid, IPC_RMID, NULL);
+  msgctl(msgid1, IPC_RMID, NULL);
+  msgctl(msgid2, IPC_RMID, NULL);
   exit(0);
 }
