b7200e46cde509311458fadbb9eb1e61c0ce9608
jiwook021 <jiwook021@gmail.com>
2022-07-19 21:18:33 -0700

    fe

--- Process_1.c
+++ Process_1.c
@@ -15,9 +15,6 @@
 #include <sys/stat.h>
 
 #define MAX 100
-
-
-
 #define BILLION  1000000000L;
 
 struct timespec start, stop, init_start, thread_stop;
@@ -37,63 +34,12 @@ char Time_message[10];
 char current_time[20] = "Process Start Time: ";
 char Message_Received[30] = "s     Message Received: ";
 
+//function call
 void  INThandler(int);
-
-int msleep(long msec)
-{
-  struct timespec ts;
-  int res;
-
-   if (msec < 0)
-  {
-    errno = EINVAL;
-    return -1;
-  }
-
-  ts.tv_sec = msec / 1000;
-  ts.tv_nsec = (msec % 1000) * 1000000;
-
-  do 
-  {
-        res = nanosleep(&ts, &ts);
-  } 
-  while (res && errno == EINTR);
-    return res;
-}
-
-char *my_itoa(int num, char *str)
-{
-  if(str == NULL)
-  {
-    return NULL;
-  }
-  
-  sprintf(str, "%d", num);
-  return str;
-}
-
-
-static void * threadFunc(void *arg)
-{
-  if (clock_gettime(CLOCK_REALTIME, &init_start) == -1 ) 
-    {
-      perror( "clock gettime" );
-      exit( EXIT_FAILURE );
-    }
-  while(1)
-    {
-      if (clock_gettime(CLOCK_REALTIME, &thread_stop) == -1 ) 
-      {
-        perror( "clock gettime" );
-        exit( EXIT_FAILURE );
-      }
-      msleep(900);
-      
-    Total_time = ( thread_stop.tv_sec - init_start.tv_sec )+ (float)( thread_stop.tv_nsec - init_start.tv_nsec )/BILLION; 
-    printf("\nA_Total_Timer:%.4lf", Total_time); 
-    }
-  return (void *) arg;
-}
+void ALRMhandler();
+int msleep(long); 
+char *my_itoa(int, char*);
+static void * threadFunc(void *arg);
 
 struct mesg_buffer1 {
     long mesg_type;
@@ -122,52 +68,57 @@ int main()
   msgid1 = msgget(key1, 0666 | IPC_CREAT);  
   msgid2 = msgget(key2, 0666 | IPC_CREAT);
   
-  if((msgid1==-1) | (msgid2 ==-1))
+  if((-1==msgid1) | (-1==msgid2))
   {
-    printf("Msgget on A Failed\n");
+    perror("Msgget on A Failed\n");
   }
     
   message1.mesg_type = 1;
   message1.number = 1;
   message1.pid = getpid();
   message1.qid = msgid1;
+
   signal(SIGINT, INThandler);
-      
+  signal(SIGALRM, ALRMhandler);
+
+  alarm(5);
+
   s1 = pthread_create(&t1, NULL, threadFunc, &x);
-    if (s1 != 0)
-      printf("Error pthread_create");
+    if (0 != s1)
+      perror("Error pthread_create\n");
 
   fd1 = open("Process_1.log", O_WRONLY | O_CREAT |  O_APPEND, S_IRUSR | S_IWUSR);
-    if (fd1 == -1)
-      printf("Error opening file");
+    if (-1 == fd1)
+      perror("Error opening file\n");
       
   char first[20] = "Process_1 Open\n";
-  write(fd1, first, strlen(first));
+  if(-1 == write(fd1, first, strlen(first)))
+  {
+    perror("Write Failed");
+  }
 
   while(1)
   {
-    if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) 
+
+    if(-1 == clock_gettime( CLOCK_REALTIME, &start)) 
     {
-      perror( "clock gettime" );
-      exit( EXIT_FAILURE );
+      perror( "clock gettime\n");
     }
     msleep(900);
     message1.number++;
     printf("\n");
     
-    if (msgsnd((int)msgid1, (void*)&message1, (size_t)sizeof(message1)-(size_t)sizeof(message1.mesg_type), (int)0) == -1)
+    if (-1 == msgsnd((int)msgid1, (void*)&message1, (size_t)sizeof(message1)-(size_t)sizeof(message1.mesg_type), (int)0))
     {
       printf("msgnd on A Failed %s\n", strerror(errno));       
     } 
     
-    if(msgrcv((int)msgid2, (void*)&message2, (size_t)sizeof(message2)-(size_t)sizeof(message2.mesg_type), 1, 0) == -1)
-    {
-      printf("Msgrcv on A Failed  %s\n", strerror(errno));    
-    } 
+    msgrcv((int)msgid2, (void*)&message2, (size_t)sizeof(message2)-(size_t)sizeof(message2.mesg_type), 1, 0); 
+   
     
     printf("\n");
         
-    if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) 
+    if(-1 == clock_gettime( CLOCK_REALTIME, &stop)) 
     {
       perror( "clock gettime" );
       exit( EXIT_FAILURE );
@@ -181,33 +132,53 @@ int main()
     my_itoa((int)message2.number, message);
     my_itoa((int)Total_time, Time_message);
 
-    write(fd1, current_time, strlen(current_time));     
-    write(fd1, Time_message, strlen(Time_message));
-    write(fd1, Message_Received, strlen(Message_Received));
-    write(fd1, message, strlen(message));
-    write(fd1, nl, strlen(nl));
+    if( -1 == write(fd1, current_time, strlen(current_time)))
+    {
+      perror("Write error");
+    }     
+    if( -1 == write(fd1, Time_message, strlen(Time_message)))
+    {
+      perror("Write error");
+    }
+    if( -1 == write(fd1, Message_Received, strlen(Message_Received)))
+    {
+      perror("Write error");
+    }
+    if( -1 == write(fd1, message, strlen(message)))
+    {
+      perror("Write error");
+    }
+    if( -1 == write(fd1, nl, strlen(nl)))
+    {
+      perror("Write error");
+    }
   }
 
   if(-1 == msgctl(msgid1, IPC_RMID, NULL))
   {
-    printf("msgctl on msgid1 Failed  %s\n", strerror(errno));    
+    perror("msgctl on msgid1 Failed\n");    
   }
   
   if(-1 == msgctl(msgid2, IPC_RMID, NULL))
   {
-    printf("msgctl on msgid2 Failed  %s\n", strerror(errno));    
+    perror("msgctl on msgid2 Failed  \n");    
   }
      
   s1 = pthread_join(t1, &res1);
   
-  if (s1 != 0)
+  if (0!= s1)
   {
-    printf("Thread Join on Process_1 Failed");
+    perror("Thread Join on Process_1 Failed\n");
   }
   return 0;
    
 }
-   
+
+void ALRMhandler()
+{
+  printf("\nAlarm Signal Generated!");
+  alarm(5);
+}
      
     
 void INThandler(int sig)
@@ -215,24 +186,80 @@ void INThandler(int sig)
 
   if(-1 == msgctl(msgid1, IPC_RMID, NULL))
   {
-    printf("msgctl on msgid1 Failed  %s\n", strerror(errno));    
+    perror("msgctl on msgid1 Failed\n");    
   }
     if(-1 == msgctl(msgid2, IPC_RMID, NULL))
   {
-    printf("msgctl on msgid2 Failed  %s\n", strerror(errno));    
+    perror("msgctl on msgid2 Failed\n");    
   }
   
   if (-1 == close(fd1))
   {
-    printf("Close Failed! %s\n", strerror(errno));      
+    perror("Close Failed!\n");      
   }
 
   if ( -1 == kill(message2.pid,SIGTERM))
   {
-    printf("Kill on Message2.pid Failed  %s\n", strerror(errno));    
+    perror("Kill on Message2.pid Failed\n");    
   }
  
-
-   
   exit(0);
+}
+
+
+int msleep(long msec)
+{
+  struct timespec ts;
+  int res;
+
+   if (msec < 0)
+  {
+    errno = EINVAL;
+    return -1;
+  }
+
+  ts.tv_sec = msec / 1000;
+  ts.tv_nsec = (msec % 1000) * 1000000;
+
+  do 
+  {
+    res = nanosleep(&ts, &ts);
+  } 
+  while (res && errno == EINTR);
+    return res;
+}
+
+char *my_itoa(int num, char *str)
+{
+  if(str == NULL)
+  {
+    return NULL;
+  }
+  
+  sprintf(str, "%d", num);
+  return str;
+}
+
+
+static void * threadFunc(void *arg)
+{
+  if (-1 == clock_gettime(CLOCK_REALTIME, &init_start)) 
+    {
+      perror( "clock gettime" );
+      exit(EXIT_FAILURE);
+    }
+  while(1)
+    {
+      if (-1== clock_gettime(CLOCK_REALTIME, &thread_stop)) 
+      {
+        perror( "clock gettime" );
+        exit( EXIT_FAILURE );
+      }
+      msleep(900);
+  
+    Total_time = ( thread_stop.tv_sec - init_start.tv_sec )+ (float)( thread_stop.tv_nsec - init_start.tv_nsec )/BILLION; 
+    printf("\nA_Total_Timer:%.4lf", Total_time); 
+
+    }
+  return (void *) arg;
 }
