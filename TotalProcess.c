5169a67a0bcf180d864c2a6c4431240a6c92f612
jiwook021 <jiwook021@gmail.com>
2022-07-20 19:25:47 -0700

    gr

--- TotalProcess.c
+++ TotalProcess.c
@@ -5,9 +5,28 @@
 int main()
 {
 
+    const char * command0 = "ls -al"; 
     const char * command1 = "./Process_1 &";
-    system(command1);
-    sleep(2);
     const char * command2 = "./Process_2";
+     
+    system(command0);
+   
+    sleep(2);
+    system(command1);
+   
+    
+    sleep(2);
+
+    // if (return1==-1||return1 ==127)
+    // {
+    //     exit(0);
+    // }
+    
     system(command2);
+
+    // if (return2==-1||return2 ==127)
+    // { 
+    //     printf("Error for Process_2");
+    //     exit(0);
+    // }
 }
